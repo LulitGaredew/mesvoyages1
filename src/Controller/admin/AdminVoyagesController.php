b62f1566d0a586fb8138766c58ecf0b3e8d3f7c5
LulitGaredew <lulit.garedew@live.fr>
2021-09-20 15:38:14 +0200

    phase 15: ORM (deuxième table 'environnement' et lien entre les deux tables, type association ManyToMany))

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -7,6 +7,7 @@ use App\Form\VisiteType;
 use App\Repository\VisiteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -74,4 +75,26 @@ class AdminVoyagesController extends AbstractController{
             'formvisite' => $formVisite->createView()
         ]);
     }
+    
+    /**
+     * @Route ("/admin/ajout", name="admin.voyage.ajout")
+     * @param Request $request
+     * @return Response
+     */
+    public function ajout(Request $request): Response{
+        $visite = new Visite();
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+        
+        $formVisite->handleRequest($request); 
+        if($formVisite->isSubmitted() && $formVisite->isValid()){
+            $this->om->persist($visite); 
+            $this->om->flush();
+            return $this->redirectToRoute('admin.voyages');
+        }
+        return $this->render("admin/admin.voyage.ajout.html.twig", [
+            'visite' => $visite, 
+            'formvisite' => $formVisite->createView()
+        ]);
+    }
+    
 }
